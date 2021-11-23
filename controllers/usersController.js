16d3e4797b6a8f43677073460f5569c2e1f0d82f
Abdi-01 <abdialghi@gmail.com>
2021-11-23 21:09:36 +0700

    first commit

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -9,10 +9,19 @@ module.exports = {
 
             res.status(200).send(results)
         })
-
     },
     register: (req, res) => {
+        let insertScript = `INSERT INTO users values 
+        (null,'${req.body.username}', '${req.body.email}', 
+        '${req.body.telp}', '${req.body.role}', '${req.body.status}',${req.body.usia}, 0);`
 
+        db.query(insertScript, (err, results) => {
+            if (err) {
+                console.log(err)
+                res.status(500).send(err)
+            }
+            res.status(200).send({ messages: "Register success ✅", results })
+        })
     },
     login: (req, res) => {
 
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -3,5 +3,6 @@ const { usersController } = require('../controllers')
 const router = express.Router()
 
 router.get('/get', usersController.getData);
+router.post('/register', usersController.register)
 
 module.exports = router
