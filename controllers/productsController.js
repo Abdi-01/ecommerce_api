a8b39576226888407f9c497d62e98c51ac3a8090
Abdi-01 <abdialghi@gmail.com>
2021-11-25 21:14:50 +0700

    add product controller

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -5,8 +5,10 @@ module.exports = {
         // 1. Ambil data dari table products
         // 2. Ambil data dari product_image
         // 3. looping results dari table products dan product image untuk digabungkan berdasarkan foreign key
+        // CARA BEST PRACTICE MENGGUNAKAN PROMISIFY
         try {
-            let sqlGetProducts = `Select * from products;`;
+            console.log(req.query.idproduct)
+            let sqlGetProducts = `Select * from products ${req.query.idproduct ? `WHERE idproduct=${req.query.idproduct}` : ""};`;
             let sqlGetProductImage = `Select * from product_image;`;
 
             let getProducts = await dbQuery(sqlGetProducts);
@@ -56,5 +58,26 @@ module.exports = {
         //     })
 
         // })
+    },
+    addProduct: async (req, res) => {
+        try {
+            console.log(req.body)
+            let { name, brand, category, stock, price, description, images } = req.body;
+
+            let sqlProduct = `INSERT INTO products values (null, '${name}', '${brand}', '${category}', '${description}', ${stock}, ${price}, 'ready');`
+
+            let insertProduct = await dbQuery(sqlProduct);
+            console.log(insertId)
+            if (insertProduct.insertId) {
+                for (let i = 0; i < images.length; i++) {
+                    await dbQuery(`INSERT INTO product_image values (null, ${insertProduct.insertId}, '${images[i]}')`)
+                }
+
+                res.status(200).send({ message: "Add product success ✅" })
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(500).send(error);
+        }
     }
 }
--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -3,5 +3,6 @@ const { productsController } = require('../controllers')
 const router = express.Router()
 
 router.get('/get', productsController.getProducts)
+router.post('/add', productsController.addProduct)
 
 module.exports = router
