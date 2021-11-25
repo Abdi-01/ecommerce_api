b3ea541384cb8c1e66d8a247bfcb428a5b6c8570
Abdi-01 <abdialghi@gmail.com>
2021-11-25 20:00:24 +0700

    keep-login api

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,5 @@
 const { db } = require('../config/database')
+const Crypto = require('crypto')
 
 module.exports = {
     getData: (req, res) => {
@@ -11,8 +12,10 @@ module.exports = {
         })
     },
     register: (req, res) => {
+        // enkripsi password/hashing password
+        let hashPassword = Crypto.createHmac("sha256", "key_password").update(req.body.password).digest("hex")
         let insertScript = `INSERT INTO users values 
-        (null,'${req.body.username}', '${req.body.email}', '${req.body.password}', 
+        (null,'${req.body.username}', '${req.body.email}', '${hashPassword}', 
         '${req.body.telp}', 'user', 'Unverified');`
 
         db.query(insertScript, (err, results) => {
@@ -24,6 +27,21 @@ module.exports = {
         })
     },
     login: (req, res) => {
+        let hashPassword = Crypto.createHmac("sha256","key_password").update(req.body.password).digest("hex")
+        let loginScript = `Select * from users WHERE email = '${req.body.email}' AND password = '${hashPassword}';`
+
+        db.query(loginScript, (err, results) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send(err);
+            }
+
+            // delete results[0].password;
+
+            res.status(200).send({ messages: "Login Success ✅", results })
+        })
+    },
+    keepLogin: (req, res) => {
         let loginScript = `Select * from users WHERE email = '${req.body.email}' AND password = '${req.body.password}';`
 
         db.query(loginScript, (err, results) => {
@@ -31,6 +49,8 @@ module.exports = {
                 console.log(err);
                 res.status(500).send(err);
             }
+
+            // delete results[0].password;
 
             res.status(200).send({ messages: "Login Success ✅", results })
         })
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -5,5 +5,6 @@ const router = express.Router()
 router.get('/get', usersController.getData);
 router.post('/register', usersController.register)
 router.post('/login', usersController.login);
+router.post('/keep-login', usersController.keepLogin);
 
 module.exports = router
